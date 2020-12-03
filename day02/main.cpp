4555b6050af3b00c2b4a0ef46f3853594ad193a5
Charney <charneykaye@gmail.com>
2020-12-02 21:44:46 -0800

    Draft of Day 03

--- day02/main.cpp
+++ day02/main.cpp
@@ -1,19 +1,17 @@
 #include<iostream>
-#include <fstream>
 #include <bits/stdc++.h>
 
 using namespace std;
 
-regex rgxPassword ("\\s([a-z]+)$");
-regex rgxLetter ("([a-z]):");
-regex rgxMin ("(\\d+)\\-");
-regex rgxMax ("\\-(\\d+)");
+regex rgxPassword("\\s([a-z]+)$");
+regex rgxLetter("([a-z]):");
+regex rgxMin("(\\d+)\\-");
+regex rgxMax("\\-(\\d+)");
 
 /*
 Get password from <theorem>
 */
-string getPassword(string theorem)
-{
+string getPassword(string theorem) {
     smatch match;
     regex_search(theorem, match, rgxPassword);
     return match[1].str();
@@ -22,8 +20,7 @@ string getPassword(string theorem)
 /*
 Get required letter from <theorem>
 */
-char getLetter(string theorem)
-{
+char getLetter(string theorem) {
     smatch match;
     regex_search(theorem, match, rgxLetter);
     return match[1].str()[0];
@@ -32,8 +29,7 @@ char getLetter(string theorem)
 /*
 Get minimum occurrences of required letter from <theorem>
 */
-int getNumOne(string theorem)
-{
+int getNumOne(string theorem) {
     smatch match;
     regex_search(theorem, match, rgxMin);
     return stoi(match[1].str());
@@ -42,21 +38,19 @@ int getNumOne(string theorem)
 /*
 Get maximum occurrences of required letter from <theorem>
 */
-int getNumTwo(string theorem)
-{
+int getNumTwo(string theorem) {
     smatch match;
     regex_search(theorem, match, rgxMax);
     return stoi(match[1].str());
 }
 
 /*
-Get number of occurences of a given <letter> in <password>
+Get number of occurrences of a given <letter> in <password>
 */
-int getOccurences(string password, char letter)
-{
+int getOccurrences(string password, char letter) {
     int r = 0;
-    for (int i=0; i< password.size(); i++)
-        if (password[i]==letter) 
+    for (int i = 0; i < password.size(); i++)
+        if (password[i] == letter)
             r++;
     return r;
 }
@@ -64,8 +58,7 @@ int getOccurences(string password, char letter)
 /*
 Part 1 using <theorems>
 */
-void part1(vector<string> *theorems)
-{
+void part1(vector<string> *theorems) {
     cout << endl << "Part 1" << endl;
     int totalValid = 0;
     for (auto theorem : *theorems) {
@@ -73,8 +66,8 @@ void part1(vector<string> *theorems)
         char letter = getLetter(theorem);
         int min = getNumOne(theorem);
         int max = getNumTwo(theorem);
-        int occurences = getOccurences(password, letter);
-        if (min <= occurences && occurences <= max)
+        int occurrences = getOccurrences(password, letter);
+        if (min <= occurrences && occurrences <= max)
             totalValid++;
 
     }
@@ -84,8 +77,7 @@ void part1(vector<string> *theorems)
 /*
 Part 2 using <theorems>
 */
-void part2(vector<string> *theorems)
-{
+void part2(vector<string> *theorems) {
     cout << endl << "Part 2" << endl;
     int totalValid = 0;
     for (auto theorem : *theorems) {
@@ -93,9 +85,9 @@ void part2(vector<string> *theorems)
         char letter = getLetter(theorem);
         int posA = getNumOne(theorem);
         int posB = getNumTwo(theorem);
-        if ((password[posA-1] == letter && password[posB-1] != letter) ||
-            (password[posA-1] != letter && password[posB-1] == letter)) 
-                totalValid++;
+        if ((password[posA - 1] == letter && password[posB - 1] != letter) ||
+            (password[posA - 1] != letter && password[posB - 1] == letter))
+            totalValid++;
     }
     cout << "Found " << totalValid << " theorems to be valid" << endl;
 }
@@ -103,13 +95,12 @@ void part2(vector<string> *theorems)
 /*
 Bootstrap reads inputs from file
 */
-int main()
-{
+int main() {
     vector<string> theorems;
 
     vector<int> amounts;
     string line;
-    while (std::getline(std::cin, line)) 
+    while (std::getline(std::cin, line))
         theorems.push_back(line);
 
     part1(&theorems);
---
+++ day03/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <fstream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+Bootstrap reads inputs from file
+*/
+int main()
+{
+    vector<string> lines;
+    vector<int> amounts;
+    string line;
+    while (std::getline(std::cin, line)) 
+        theorems.push_back(line);
+
+    return 0;
+}

db6dcc6e2472363f84cfbb179519b8f30f61efc8
Charney <charneykaye@gmail.com>
2020-12-03 23:09:05 -0800

    Solved Day 4 Part 2

--- day04/main.cpp
+++ day04/main.cpp
@@ -9,9 +9,19 @@ Processor parses one theorem and provides computed results
  */
 class Processor {
     regex rgxKVPair;
+    regex rgxHeightCm;
+    regex rgxHeightIn;
+    regex rgxHairColor;
+    regex rgxPassportId;
+    vector<string> validEyeColors;
 public:
     Processor();
 
+    /**
+     * Parse KV pairs from a theorem into a map
+     * @param theorem to parse
+     * @return  map of keys to values
+     */
     map<string, string> parseKeyValues(const string &theorem) {
         map<string, string> keyValues;
         auto t_begin = std::sregex_iterator(theorem.begin(), theorem.end(), rgxKVPair);
@@ -23,19 +33,123 @@ public:
         return keyValues;
     }
 
-};
+    /**
+     Test if fields are present
+     @param map  to test
+     @return  true if fields are present
+     */
+    static bool hasFields(const map<string, string> &map) {
+        if (0 == map.count("byr")) return false; // (Birth Year)
+        if (0 == map.count("iyr")) return false; // (Issue Year)
+        if (0 == map.count("eyr")) return false; // (Expiration Year)
+        if (0 == map.count("hgt")) return false; // (Height)
+        if (0 == map.count("hcl")) return false; // (Hair Color)
+        if (0 == map.count("ecl")) return false; // (Eye Color)
+        if (0 == map.count("pid")) return false; // (Passport ID)
+        // ignore  if (!map.contains("cid")) return false; // (Country ID)
+        return true;
+    }
 
-bool isValid(const map<string, string> &map) {
-    if (0 == map.count("byr")) return false; // (Birth Year)
-    if (0 == map.count("iyr")) return false; // (Issue Year)
-    if (0 == map.count("eyr")) return false; // (Expiration Year)
-    if (0 == map.count("hgt")) return false; // (Height)
-    if (0 == map.count("hcl")) return false; // (Hair Color)
-    if (0 == map.count("ecl")) return false; // (Eye Color)
-    if (0 == map.count("pid")) return false; // (Passport ID)
-// ignore  if (!map.contains("cid")) return false; // (Country ID)
-    return true;
-}
+    /**
+     Test if fields are present and valid
+     @param map  to test
+     @return  true if fields are present
+     */
+    bool hasValidFields(const map<string, string> &map) {
+        // byr (Birth Year) - four digits; at least 1920 and at most 2002.
+        int byr = stoi(map.at("byr"));
+        if (!(byr >= 1920 && byr <= 2002))
+            return false;
+
+        // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+        int iyr = stoi(map.at("iyr"));
+        if (!(iyr >= 2010 && iyr <= 2020))
+            return false;
+
+        // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+        int eyr = stoi(map.at("eyr"));
+        if (!(eyr >= 2020 && eyr <= 2030))
+            return false;
+
+        // hgt (Height) - a number followed by either cm or in:
+        // If cm, the number must be at least 150 and at most 193.
+        // If in, the number must be at least 59 and at most 76.
+        if (!isValidHeight(map.at("hgt")))
+            return false;
+
+        // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+        if (!isValidHairColor(map.at("hcl")))
+            return false;
+
+        // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+        if (find(validEyeColors.begin(), validEyeColors.end(), map.at("ecl")) == validEyeColors.end())
+            return false;
+
+        // pid (Passport ID) - a nine-digit number, including leading zeroes.
+        if (!isValidPassportId(map.at("pid")))
+            return false;
+
+        // cid (Country ID) - ignored, missing or not.
+
+        return true;
+    }
+
+    /**
+     Test if passportId is valid
+     pid (Passport ID) - a nine-digit number, including leading zeroes.
+
+     @param passportId  to test
+     @return  true if valid passportId
+     */
+    bool isValidPassportId(const string &passportId) {
+        smatch match;
+        regex_search(passportId, match, rgxPassportId);
+        return !match.empty();
+    }
+
+    /**
+     Test if hairColor is valid
+     hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+
+     @param hairColor  to test
+     @return  true if valid hairColor
+     */
+    bool isValidHairColor(const string &hairColor) {
+        smatch match;
+        regex_search(hairColor, match, rgxHairColor);
+        return !match.empty();
+    }
+
+    /**
+     Test if height is valid
+     hgt (Height) - a number followed by either cm or in:
+     If cm, the number must be at least 150 and at most 193.
+     If in, the number must be at least 59 and at most 76.
+
+     @param height  to test
+     @return  true if valid height
+     */
+    bool isValidHeight(const string &height) {
+        smatch match;
+
+        // If cm, the number must be at least 150 and at most 193.
+        regex_search(height, match, rgxHeightCm);
+        if (!match.empty()) {
+            int h = stoi(match[1].str());
+            return h >= 150 && h <= 193;
+        }
+
+        // If in, the number must be at least 59 and at most 76.
+        regex_search(height, match, rgxHeightIn);
+        if (!match.empty()) {
+            int h = stoi(match[1].str());
+            return h >= 59 && h <= 76;
+        }
+
+        return false;
+    }
+
+};
 
 /**
 Construct regexes
@@ -43,21 +157,45 @@ Construct regexes
  */
 Processor::Processor() {
     rgxKVPair = regex(R"(\b([a-z]+):([^\s]+))");
+    rgxHeightCm = regex(R"(([0-9]+)cm)");
+    rgxHeightIn = regex(R"(([0-9]+)in)");
+    rgxHairColor = regex(R"(#([a-f0-9]{6}))");
+    rgxPassportId = regex(R"(^([0-9]{9})$)");
+    validEyeColors = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
 }
 
 /**
-Solve: Seek a vector until we leave the bottom of the processor
+Solve Part 1
  */
-unsigned int solve(Processor *processor, vector<string> *theorems) {
-    int valid = 0;
+void solvePart1(Processor *processor, vector<string> *theorems) {
+    cout << "Part 1: Are fields present?" << endl;
+    int valid = 0, invalid = 0;
     map<string, string> keyValues;
     for (const auto &theorem : *theorems) {
         keyValues = processor->parseKeyValues(theorem);
-        if (isValid(keyValues))
+        if (Processor::hasFields(keyValues))
             valid++;
+        else
+            invalid++;
     }
-    cout << "Processor found " << valid << " theorems to be valid." << endl << endl;
-    return valid;
+    cout << "Processor found " << valid << " theorems to be valid and " << invalid << " invalid." << endl << endl;
+}
+
+/**
+Solve Part 2
+ */
+void solvePart2(Processor *processor, vector<string> *theorems) {
+    cout << "Part 2: Are fields present and valid?" << endl;
+    int valid = 0, invalid = 0;
+    map<string, string> keyValues;
+    for (const auto &theorem : *theorems) {
+        keyValues = processor->parseKeyValues(theorem);
+        if (Processor::hasFields(keyValues) && processor->hasValidFields(keyValues))
+            valid++;
+        else
+            invalid++;
+    }
+    cout << "Processor found " << valid << " theorems to be valid and " << invalid << " invalid." << endl << endl;
 }
 
 /**
@@ -93,7 +231,8 @@ int main() {
 
     Processor processor;
 
-    solve(&processor, &theorems);
+    solvePart1(&processor, &theorems);
+    solvePart2(&processor, &theorems);
     cout << endl;
 
     return 0;
